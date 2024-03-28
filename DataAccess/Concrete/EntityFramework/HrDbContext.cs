77937942061814ff5c9b84f8e323fec2c84d6624
altinbarin <furkanaltinbarin@gmail.com>
2024-03-28 20:40:47 +0300

    first commit

--- DataAccess/Concrete/EntityFramework/HrDbContext.cs
+++ DataAccess/Concrete/EntityFramework/HrDbContext.cs
@@ -1,10 +1,7 @@
 ﻿using Core.Entities.Concrete;
 using Entities.Concrete;
 using Microsoft.EntityFrameworkCore;
-using System;
 using System.Reflection;
-using static System.Net.Mime.MediaTypeNames;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 
 namespace DataAccess.Concrete.EntityFramework
@@ -13,7 +10,7 @@ namespace DataAccess.Concrete.EntityFramework
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source=toall.database.windows.net;Initial Catalog=HRWebDb;User ID=furkanhakan;Password=toall123.;Connect Timeout=30;Encrypt=True;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-PE0VBS8;Database = HRWebDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
             //Database = HRWebDb
             //Data Source=DESKTOP-PE0VBS8;Database = HRWebDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False
             //Data Source = toall.database.windows.net; Initial Catalog = HRWebDb; User ID = furkanhakan; Password = toall123.; Connect Timeout = 30; Encrypt = True; Trust Server Certificate = False; Application Intent = ReadWrite; Multi Subnet Failover = False
